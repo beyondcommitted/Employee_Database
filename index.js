99c9b4a333b7c1a330e61fed0564ec69aff6e081
Breon Sparrow <beyond.committed@gmail.com>
2021-03-29 20:46:13 -0700

    choose to add delete or exit added

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
 const connection = require("./db");
+const inquirer = require("inquirer");
 
-connection.query("SELECT * FROM employees")
-.then(function(data){
-    console.table(data);
-})
-    .catch(function (err) {
-        console.log(err)
-    })
-connection.query("SELECT * FROM emp_role")
-.then(function(data){
-    console.table(data);
-})
-    .catch(function (err) {
-        console.log(err)
-    })
-connection.query("SELECT * FROM department")
-.then(function(data){
-    console.table(data);
-})
-    .catch(function (err) {
-        console.log(err)
-    })
+init();
+
+async function init() {
+  const { addOrDelete} = await inquirer.prompt({
+    name: "addEmployee",
+    type: "list",
+    message: "Would you like to [ADD] or [DELETE] an employee?",
+    choices: ["ADD", "DELETE", "EXIT"],
+  });
+
+  switch (addOrDelete) {
+    case "ADD":
+      // Post an item
+      addEmployee();
+      break;
+    case "DELETE":
+        deleteEmployee();
+        break;
+        default:
+            process.exit(0);  
+  }};
+      async function addEmployee() {
+        const { first_name, last_name } = await inquirer.prompt([
+          {
+            name: "first_name",
+            message: "What is the first name of the employee you would like to add?",
+            type: "input",
+          },
+          {
+            name: "last_name",
+            message: "What is the last name of the employee you would like to add?",
+            type: "input",
+          },
+        ]);
+      
+// insert the item into our database
+        const data = await connection.query("INSERT INTO employees SET ?", {
+            first_name: first_name, 
+            last_name: last_name,
+        });
+        console.log(data + "Insertred Successfully");    
+
+        const deleteEmployee = () => {
+            console.log("Employee being deleted");
+            connection.query(
+              'DELETE FROM employees WHERE ?',
+              {
+                first_name: "first_name",
+                last_name: "last_name",
+              },
+              (error, response) => {
+                if (error) throw error;
+                console.log(`${res.data} products deleted!\n`);
+                deleteEmployee();
+              }
+            );
+          };
+
+
+
+        }
